4e03427c97bf4f3b23010b4c96b5b5c1a901fe4c
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-31 21:46:28 -0800

    calls base components in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Main from "./components/Layout/Main/Main.js";
+import Nav from "./components/Layout/Nav/Nav.js";
 
 function App() {
     return (
         <div className="App">
             <h1>App</h1>
+            <Nav />
+            <Main />
         </div>
     );
 }
