1c45718ebdadf42ad59263b064918247903d6e83
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-02-05 11:48:12 -0800

    adds styles for layout to main tag

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -3,12 +3,18 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { useUser } from "../../../context/UserContext.js";
 import Auth from "../../Pages/Auth/Auth.js";
 import Home from "../../Pages/Home/Home.js";
-import RecipeForm from "../../Pages/Recipes/RecipeForm.js";
+import RecipeForm from "../../Pages/Recipes/RecipeForm/RecipeForm.js";
 
 export default function Main() {
     const { user } = useUser();
     return (
-        <main>
+        <main
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
             <Routes>
                 <Route path="/auth/:type" element={<Auth />} />
                 <Route path="/home" element={<Home />} />
