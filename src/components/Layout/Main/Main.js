eae20bdb8d66f6ce4fddfa9e8a0ad5cac40788ec
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-02-04 17:15:57 -0800

    adds route to first 3 pages auth home and recipes

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useUser } from "../../../context/UserContext.js";
+import Auth from "../../Pages/Auth/Auth.js";
 import Home from "../../Pages/Home/Home.js";
+import RecipeForm from "../../Pages/Recipes/RecipeForm.js";
 
 export default function Main() {
+    const { user } = useUser();
     return (
         <main>
-            <Home />
+            <Routes>
+                <Route path="/auth/:type" element={<Auth />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/recipe-editor" element={<RecipeForm />} />
+                <Route
+                    path="/"
+                    element={user ? <Home /> : <Navigate to="/auth/sign-in" />}
+                />
+            </Routes>
         </main>
     );
 }
