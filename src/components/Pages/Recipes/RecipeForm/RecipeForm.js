cd539e9d0a414c766baef6b824d8a59950db0f0e
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-02-05 19:02:14 -0800

    adds useTheme and applies to recipe form elems

--- src/components/Pages/Recipes/RecipeForm/RecipeForm.js
+++ src/components/Pages/Recipes/RecipeForm/RecipeForm.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../../../../context/UserContext.js";
 import { InputLabel, FormControl, OutlinedInput } from "@mui/material/";
+import { useTheme } from "@mui/material/styles";
 
 import Box from "@mui/material/Box";
+import IngredientList from "./FormComponents/IngredientList.js";
 
 export default function RecipeForm() {
     const { user } = useUser();
+
+    const theme = useTheme();
 
     if (!user) {
         return <Navigate to="/auth/sign-in" />;
@@ -28,26 +32,79 @@ export default function RecipeForm() {
                 justifyContent: "center",
                 border: "1px solid black",
                 borderRadius: "10px",
+                backgroundColor: theme.palette.primary.light,
             }}
         >
             <h3>Add A Recipe</h3>
+
             {/* DISH NAME HERE */}
-            <FormControl sx={{ m: 1, width: "90%" }} variant="outlined">
+            <FormControl
+                sx={{
+                    "& .MuiOutlinedInput-root": {
+                        "&.Mui-focused fieldset": {
+                            borderColor: theme.palette.primary.dark,
+                        },
+                    },
+                    "& label.Mui-focused": {
+                        position: "absolute",
+                        left: "-5px",
+                        margin: "0",
+                        padding: "0 5px",
+                        borderRadius: "5px",
+                        backgroundColor: theme.palette.background.paper,
+                        color: theme.palette.primary.contrastText,
+                        fontWeight: "700",
+                    },
+                    m: 1,
+                    width: "80%",
+                }}
+                variant="outlined"
+            >
                 <InputLabel margin="dense" htmlFor="outlined-dish-name">
                     Dish Name
                 </InputLabel>
                 <OutlinedInput
+                    sx={{ backgroundColor: theme.palette.background.paper }}
                     name="dish-name"
                     id="outlined-dish-name"
                     type="text"
-                    label="dish-name"
+                    label="Dish Name"
                 />
             </FormControl>
+
             {/* INGREDIENTS HERE */}
-            <FormControl sx={{ m: 1, width: "90%" }}>
+            <FormControl
+                sx={{
+                    "& .MuiOutlinedInput-root": {
+                        "&.Mui-focused fieldset": {
+                            borderColor: theme.palette.primary.dark,
+                        },
+                    },
+                    "& label.Mui-focused": {
+                        position: "absolute",
+                        left: "-5px",
+                        margin: "0",
+                        padding: "0 5px",
+                        borderRadius: "5px",
+                        backgroundColor: theme.palette.background.paper,
+                        color: theme.palette.primary.contrastText,
+                        fontWeight: "700",
+                    },
+                    m: 1,
+                    width: "80%",
+                }}
+            >
                 <InputLabel htmlFor="ingredient">Ingredient</InputLabel>
-                <OutlinedInput id="ingredient" type="text" label="ingredient" />
+                <OutlinedInput
+                    sx={{ backgroundColor: theme.palette.background.paper }}
+                    id="ingredient"
+                    type="text"
+                    label="Ingredient"
+                />
             </FormControl>
+            <Box>
+                <IngredientList />
+            </Box>
         </Box>
     );
 }
