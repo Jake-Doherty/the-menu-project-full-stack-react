f4cd1ec37f8ca28cd74948eab1e3031ca1ab3cbc
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-07 16:10:36 -0800

    creates service function for images to sb

--- src/services/profile.js
+++ src/services/profile.js
@@ -1,6 +1,7 @@
 import { client, checkError } from "./client.js";
 
-export async function getProfile(userId) {
+export async function getProfile(userId, profile) {
+    if (profile) return profile;
     const response = await client
         .from("profiles")
         .select("*")
@@ -10,30 +11,49 @@ export async function getProfile(userId) {
     return checkError(response);
 }
 
-export async function insertProfile(userId, { username, bio }) {
-    console.log("updateProfile args", userId, username, bio);
-
+export async function insertProfile(userId, { username, bio, profileAvatar }) {
     const response = await client
         .from("profiles")
-        .insert({ display_name: username, bio: bio })
+        .insert({
+            display_name: username,
+            bio: bio,
+            avatar_image_url: profileAvatar,
+        })
         .match({ id: userId })
         .maybeSingle();
-
-    console.log("updateProfile response", response);
 
     return checkError(response);
 }
 
-export async function updateProfile(userId, { username, bio }) {
-    console.log("updateProfile args", userId, username, bio);
-
+export async function updateProfile(userId, { username, bio, profileAvatar }) {
     const response = await client
         .from("profiles")
-        .update({ display_name: username, bio: bio })
+        .update({
+            display_name: username,
+            bio: bio,
+            avatar_image_url: profileAvatar,
+        })
         .match({ id: userId })
         .maybeSingle();
 
-    console.log("updateProfile response", response);
-
     return checkError(response);
+}
+
+export async function uploadImage(bucketName, imagePath, imageFile) {
+    const bucket = client.storage.from(bucketName);
+
+    const response = await bucket.upload(imagePath, imageFile, {
+        cacheControl: "3600",
+        upsert: true,
+    });
+
+    if (response.error) {
+        console.log(response.error);
+        return null;
+    }
+
+    // Construct the URL to this image:
+    const url = `${process.env.REACT_APP_SUPABASE_URL}/storage/v1/object/public/${response.data.Key}`;
+
+    return url;
 }
