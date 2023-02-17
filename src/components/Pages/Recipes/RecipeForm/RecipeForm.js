4a5138c57ffd4543bc375a5a1ffc4016c7e88a00
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-17 14:24:13 -0800

    calls custom hook for form functions

--- src/components/Pages/Recipes/RecipeForm/RecipeForm.js
+++ src/components/Pages/Recipes/RecipeForm/RecipeForm.js
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../../../../context/UserContext.js";
+
 import {
     InputLabel,
     FormControl,
@@ -8,122 +9,35 @@ import {
     Typography,
     IconButton,
     Button,
+    Box,
 } from "@mui/material/";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 import { useTheme as useMuiTheme } from "@mui/material/styles";
 
-import Box from "@mui/material/Box";
 import IngredientList from "./FormComponents/IngredientList.js";
+import useRecipeFormFunctions from "../../../../hooks/useRecipeFormFunctions.js";
 
 export default function RecipeForm() {
     const { user } = useUser();
 
     const theme = useMuiTheme();
 
-    const ingredientRef = useRef(null);
-    const instructionRef = useRef(null);
-
-    const [ingredientList, setIngredientList] = useState([
-        {
-            unit: "",
-            quantity: "",
-            ingredientName: "",
-        },
-    ]);
-
-    const [instructionList, setInstructionList] = useState([
-        {
-            step: "",
-            instruction: "",
-        },
-    ]);
+    const {
+        ingredientList,
+        ingredientRef,
+        handleAddIngredient,
+        handleIngredientInputChange,
+        instructionList,
+        instructionRef,
+        handleAddInstruction,
+        handleInstructionInputChange,
+        handleRemoveClick,
+    } = useRecipeFormFunctions();
 
     if (!user) {
         return <Navigate to="/auth/sign-in" />;
     }
-
-    const handleIngredientInputChange = (e, index) => {
-        const { name, value } = e.target;
-
-        if (name === "ingredientName") {
-            const list = [...ingredientList];
-            list[index].ingredientName = value;
-            console.log(list);
-            setIngredientList(list);
-        }
-
-        if (name === "qty") {
-            const list = [...ingredientList];
-            list[index].quantity = value;
-            console.log(list);
-            setIngredientList(list);
-        }
-
-        if (name === "unit") {
-            const list = [...ingredientList];
-            list[index].unit = value;
-            console.log(list);
-            setIngredientList(list);
-        }
-    };
-
-    const handleInstructionInputChange = (e, index) => {
-        const { value } = e.target;
-
-        const list = [...instructionList];
-        list[index].instruction = value;
-        console.log(list);
-        setInstructionList(list);
-    };
-
-    const handleAddIngredient = () => {
-        setIngredientList([
-            ...ingredientList,
-            {
-                unit: "",
-                quantity: "",
-                ingredientName: "",
-            },
-        ]);
-        setTimeout(() => {
-            ingredientRef.current?.scrollIntoView({
-                behavior: "smooth",
-                block: "end",
-            });
-        }, 50);
-    };
-
-    const handleAddInstruction = () => {
-        setInstructionList([
-            ...instructionList,
-            {
-                step: "",
-                instruction: "",
-            },
-        ]);
-        setTimeout(() => {
-            instructionRef.current?.scrollIntoView({
-                behavior: "smooth",
-                block: "end",
-            });
-        }, 50);
-    };
-
-    const handleRemoveClick = (obj, index) => {
-        if (Object.keys(obj)[0] === "unit") {
-            const list = [...ingredientList];
-            list.splice(index, 1);
-            console.log(list);
-            setIngredientList(list);
-        }
-
-        if (Object.keys(obj)[0] === "step") {
-            const list = [...instructionList];
-            list.splice(index, 1);
-            setInstructionList(list);
-        }
-    };
 
     return (
         <Box
