468e4dc7ccd4cb247037810b0b33279d029e601c
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-17 13:08:18 -0800

    abstracts out Ingredient component for readability

--- src/components/Pages/Recipes/RecipeForm/FormComponents/Ingredient.js
+++ src/components/Pages/Recipes/RecipeForm/FormComponents/Ingredient.js
@@ -1,5 +1,123 @@
 import React from "react";
+import {
+    FormControl,
+    InputLabel,
+    MenuItem,
+    OutlinedInput,
+    TextField,
+    Typography,
+} from "@mui/material";
 
-export default function Ingredient() {
-    return <div>Ingredient</div>;
+import measurements from "../../../../../measurement-data.js";
+import RemoveButton from "./RemoveButton.js";
+
+export default function Ingredient({
+    theme,
+    index,
+    ingredient,
+    ingredientRef,
+    ingredientList,
+    handleRemoveClick,
+    handleIngredientInputChange,
+}) {
+    return (
+        <div
+            ref={ingredientList.length - 1 === index ? ingredientRef : null}
+            className="ingredient"
+            style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+            }}
+        >
+            <RemoveButton
+                {...{ theme, handleRemoveClick, ingredient, index }}
+            />
+            <Typography
+                sx={{
+                    color: theme.palette.primary.contrastText,
+                }}
+                variant="h6"
+                component="h6"
+            >
+                {index + 1}.{" "}
+            </Typography>
+            <TextField
+                name="qty"
+                label="Qty."
+                inputProps={{
+                    inputMode: "numeric",
+                    pattern: "[0-99]*",
+                }}
+                sx={{
+                    width: "max(10%, 100px)",
+                    "& .MuiInputBase-root *": {
+                        borderColor: theme.palette.primary.main,
+                        color: theme.palette.primary.contrastText,
+                    },
+                }}
+                value={ingredient.quantity}
+                onChange={(e) => handleIngredientInputChange(e, index)}
+            />
+
+            <TextField
+                id="outlined-select-measurement"
+                label="Unit"
+                name="unit"
+                select
+                value={ingredient.unit}
+                onChange={(e) => handleIngredientInputChange(e, index)}
+                sx={{
+                    width: "max(10%, 125px)",
+                    "& .MuiInputBase-root *": {
+                        borderColor: theme.palette.primary.main,
+                        color: theme.palette.primary.contrastText,
+                    },
+                }}
+            >
+                {measurements.map((option) => (
+                    <MenuItem
+                        sx={{
+                            color: theme.palette.primary.contrastText,
+                        }}
+                        key={option.value}
+                        value={option.value}
+                    >
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <FormControl
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    "& .MuiInputBase-root *": {
+                        borderColor: theme.palette.primary.main,
+                        color: theme.palette.primary.contrastText,
+                    },
+                    "& .MuiOutlinedInput-root": {
+                        "&.Mui-focused fieldset": {
+                            borderColor: theme.palette.primary.main,
+                        },
+                    },
+
+                    width: "100%",
+                }}
+            >
+                <InputLabel htmlFor="ingredient">Ingredient</InputLabel>
+                <OutlinedInput
+                    id="ingredient"
+                    label="Ingredient"
+                    name="ingredientName"
+                    type="text"
+                    value={ingredient.ingredientName}
+                    onChange={(e) => handleIngredientInputChange(e, index)}
+                    sx={{
+                        width: "98%",
+                    }}
+                />
+            </FormControl>
+        </div>
+    );
 }
--- src/components/Pages/Recipes/RecipeForm/FormComponents/IngredientList.js
+++ src/components/Pages/Recipes/RecipeForm/FormComponents/IngredientList.js
@@ -1,16 +1,5 @@
 import React from "react";
-import {
-    FormControl,
-    IconButton,
-    InputLabel,
-    MenuItem,
-    OutlinedInput,
-    TextField,
-    Typography,
-} from "@mui/material";
-import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
-import measurements from "../../../../../measurement-data.js";
-// import Ingredient from "./Ingredient.js";
+import Ingredient from "./Ingredient.js";
 
 export default function IngredientList({
     theme,
@@ -38,130 +27,18 @@ export default function IngredientList({
         >
             {ingredientList.map((ingredient, index) => {
                 return (
-                    <div
-                        ref={
-                            ingredientList.length - 1 === index
-                                ? ingredientRef
-                                : null
-                        }
-                        className="ingredient"
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            alignItems: "center",
-                        }}
+                    <Ingredient
                         key={index}
-                    >
-                        <IconButton
-                            sx={{
-                                scale: "1",
-                                margin: "0.5",
-                                padding: "1",
-                            }}
-                            aria-label="delete ingredient"
-                            onClick={() => handleRemoveClick(ingredient, index)}
-                        >
-                            <RemoveCircleIcon
-                                sx={{
-                                    color: theme.palette.error.light,
-                                }}
-                            />
-                        </IconButton>
-                        <Typography
-                            sx={{
-                                color: theme.palette.primary.contrastText,
-                            }}
-                            variant="h6"
-                            component="h6"
-                        >
-                            {index + 1}.{" "}
-                        </Typography>
-                        <TextField
-                            name="qty"
-                            label="Qty."
-                            inputProps={{
-                                inputMode: "numeric",
-                                pattern: "[0-99]*",
-                            }}
-                            sx={{
-                                width: "max(10%, 100px)",
-                                "& .MuiInputBase-root *": {
-                                    borderColor: theme.palette.primary.main,
-                                    color: theme.palette.primary.contrastText,
-                                },
-                            }}
-                            value={ingredient.quantity}
-                            onChange={(e) =>
-                                handleIngredientInputChange(e, index)
-                            }
-                        />
-
-                        <TextField
-                            id="outlined-select-measurement"
-                            label="Unit"
-                            name="unit"
-                            select
-                            value={ingredient.unit}
-                            onChange={(e) =>
-                                handleIngredientInputChange(e, index)
-                            }
-                            sx={{
-                                width: "max(10%, 125px)",
-                                "& .MuiInputBase-root *": {
-                                    borderColor: theme.palette.primary.main,
-                                    color: theme.palette.primary.contrastText,
-                                },
-                            }}
-                        >
-                            {measurements.map((option) => (
-                                <MenuItem
-                                    sx={{
-                                        color: theme.palette.primary
-                                            .contrastText,
-                                    }}
-                                    key={option.value}
-                                    value={option.value}
-                                >
-                                    {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                        <FormControl
-                            sx={{
-                                display: "flex",
-                                flexDirection: "row",
-                                alignItems: "center",
-                                "& .MuiInputBase-root *": {
-                                    borderColor: theme.palette.primary.main,
-                                    color: theme.palette.primary.contrastText,
-                                },
-                                "& .MuiOutlinedInput-root": {
-                                    "&.Mui-focused fieldset": {
-                                        borderColor: theme.palette.primary.main,
-                                    },
-                                },
-
-                                width: "100%",
-                            }}
-                        >
-                            <InputLabel htmlFor="ingredient">
-                                Ingredient
-                            </InputLabel>
-                            <OutlinedInput
-                                id="ingredient"
-                                label="Ingredient"
-                                name="ingredientName"
-                                type="text"
-                                value={ingredient.ingredientName}
-                                onChange={(e) =>
-                                    handleIngredientInputChange(e, index)
-                                }
-                                sx={{
-                                    width: "98%",
-                                }}
-                            />
-                        </FormControl>
-                    </div>
+                        {...{
+                            theme,
+                            index,
+                            ingredient,
+                            ingredientList,
+                            ingredientRef,
+                            handleRemoveClick,
+                            handleIngredientInputChange,
+                        }}
+                    />
                 );
             })}
         </article>
