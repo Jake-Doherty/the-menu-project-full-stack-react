332e8288b639f1b1f0da0ff88871d5d831771c03
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-17 12:41:16 -0800

    abstracts measurement data to its own module

---
+++ src/measurement-data.js
@@ -0,0 +1,32 @@
+const measurements = [
+    {
+        value: "Cup",
+        label: "Cup",
+    },
+    {
+        value: "TBSP",
+        label: "TBSP",
+    },
+    {
+        value: "TSP",
+        label: "TSP",
+    },
+    {
+        value: "Fl Oz",
+        label: "Fl Oz",
+    },
+    {
+        value: "pt",
+        label: "Pint",
+    },
+    {
+        value: "qt",
+        label: "Quart",
+    },
+    {
+        value: "gal",
+        label: "Gallon",
+    },
+];
+
+export default measurements;
