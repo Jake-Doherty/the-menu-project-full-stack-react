cbc2a1aa367a60bc3e3e5b650505ba8c161320c2
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-17 16:09:08 -0800

    creates ingredientsHeader comp + calls AddButton

---
+++ src/components/Pages/Recipes/RecipeForm/FormComponents/IngredientsHeader.js
@@ -0,0 +1,51 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import AddButton from "./AddButton.js";
+
+export default function IngredientsHeader({ theme, handleAddIngredient }) {
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexFlow: "row nowrap",
+                width: "80%",
+                height: "40px",
+                alignItems: "center",
+                justifyContent: "space-around",
+                borderBottom: `2px solid ${theme.palette.primary.main}`,
+            }}
+        >
+            <Typography
+                variant="h6"
+                component="h6"
+                sx={{
+                    color: theme.palette.primary.contrastText,
+                }}
+            >
+                Ingredients
+            </Typography>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: "10px",
+                    margin: 1,
+                }}
+            >
+                <Typography
+                    variant="h6"
+                    component="h6"
+                    sx={{
+                        fontSize: "1rem",
+                        color: theme.palette.primary.contrastText,
+                    }}
+                >
+                    Add an Ingredient
+                </Typography>
+                <AddButton {...{ theme, handleAddIngredient }} />
+            </div>
+        </div>
+    );
+}
