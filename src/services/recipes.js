f3527e061098382384473f651cd8d8c30044a964
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-17 21:30:12 -0800

    adds in getRecipes func

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,14 +1,14 @@
 import { client, checkError } from "./client.js";
 
-// export async function getRecipes(userId, recipes) {
-//     const response = await client
-//         .from("recipes")
-//         .select("*")
-//         .match({ user_id: userId })
-//         .maybeSingle();
+export async function getRecipes(userId, recipes) {
+    const response = await client
+        .from("recipes")
+        .select("*")
+        .match({ user_id: userId })
+        .maybeSingle();
 
-//     return checkError(response);
-// }
+    return checkError(response);
+}
 
 export async function insertRecipe(
     userId,
