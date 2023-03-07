9824c36985c4bcd3f34c305abc58991406baecf3
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-03-06 20:33:42 -0800

    adds header to auth page for App name

--- src/components/Pages/Auth/Auth.js
+++ src/components/Pages/Auth/Auth.js
@@ -13,11 +13,17 @@ import Box from '@mui/material/Box';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useTheme as useMuiTheme } from '@emotion/react';
+import { Typography } from '@mui/material';
+import Image from 'mui-image';
+
+const menuIcon = require('../../../assets/images/menu_book.png');
 
 export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState('');
+
   const { type } = useParams();
   const { user, setUser } = useUser();
   const theme = useMuiTheme();
@@ -38,11 +44,76 @@ export default function Auth() {
       setUser(newUser);
     } catch (e) {
       console.error(e);
+      setError(e.message);
     }
   };
 
   return (
     <>
+      <Box
+        component={'section'}
+        sx={{
+          position: 'absolute',
+          top: '0px',
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'nowrap',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            textAlign: 'right',
+          }}
+        >
+          <Typography
+            variant="h1"
+            component="h1"
+            sx={{
+              fontSize: window.innerWidth > 600 ? '5rem' : '3rem',
+              color: theme.palette.primary.contrastText,
+              textShadow: '2px 2px 5px rgb(0 0 0 / 50%)',
+            }}
+          >
+            The
+          </Typography>
+          <Typography
+            variant="h1"
+            component="h1"
+            sx={{
+              fontSize: window.innerWidth > 600 ? '5rem' : '3rem',
+              color: theme.palette.primary.contrastText,
+              textShadow: '2px 2px 5px rgb(0 0 0 / 50%)',
+            }}
+          >
+            Menu
+          </Typography>
+          <Typography
+            variant="h1"
+            component="h1"
+            sx={{
+              fontSize: window.innerWidth > 600 ? '5rem' : '3rem',
+              color: theme.palette.primary.contrastText,
+              textShadow: '2px 2px 5px rgb(0 0 0 / 50%)',
+            }}
+          >
+            Project
+          </Typography>
+        </Box>
+        <Image
+          src={menuIcon}
+          alt="The Menu Project"
+          style={{
+            margin: window.innerWidth > 768 ? '0 0 0 -10px' : '0 0 0 0',
+            height: window.innerWidth > 768 ? '260px' : '150px',
+          }}
+        />
+      </Box>
       <Box
         sx={{
           position: 'absolute',
@@ -62,7 +133,7 @@ export default function Auth() {
           borderRadius: '10px',
           boxShadow: '5px 5px 15px 0.5px rgb(0 0 0 / 20%)',
           transition: 'all 0.3s ease-in-out',
-          backgroundColor: theme.palette.background.paper,
+          backdropFilter: 'blur(5px)',
           '&:hover': {
             boxShadow: '5px 5px 15px 2px rgb(0 0 0 / 40%)',
             transition: 'all 0.3s ease-in-out',
@@ -192,7 +263,7 @@ export default function Auth() {
             m: 1,
             width: '25ch',
             color: theme.palette.primary.contrastText,
-            backgroundColor: theme.palette.background.paper,
+            backgroundColor: error ? theme.palette.error.main : theme.palette.background.paper,
             '&:hover': {
               backgroundColor: theme.palette.primary.main,
               boxShadow: '5px 5px 10px 1px rgb(0 0 0 / 20%)',
@@ -206,6 +277,18 @@ export default function Auth() {
         >
           Submit
         </Button>
+        {error && (
+          <Typography
+            sx={{
+              color: theme.palette.error.main,
+            }}
+            variant="h6"
+            component="h6"
+            gutterBottom
+          >
+            {error}
+          </Typography>
+        )}
       </Box>
     </>
   );
